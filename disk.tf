c25c92eead50b33d8eff8cab4857bf77f5e7c724
Onanuga Oreoluwa <onagoreoluwa92@gmail.com>
2024-05-12 09:14:01 +0100

    change vm resource in datadisk attachment

--- disk.tf
+++ disk.tf
@@ -11,7 +11,7 @@ resource "azurerm_managed_disk" "data_disk" {
 resource "azurerm_virtual_machine_data_disk_attachment" "data_disk_attachment" {
   count              = var.vm_count
   managed_disk_id    = azurerm_managed_disk.data_disk[count.index].id
-  virtual_machine_id = azurerm_virtual_machine.vm[count.index].id
+  virtual_machine_id = azurerm_linux_virtual_machine.vm[count.index].id
   lun                = 0
   caching            = "ReadWrite"
 }
